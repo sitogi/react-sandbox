56df84b63a6c80bec4bd270d6071196013765cf2
sitogi <takano162@gmail.com>
2023-01-20 12:04:46 +0900

    useState をやめてリサイズ時の再レンダリングを抑制

---
+++ src/components/ResizableSidebar/Divider.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+import { Box } from '@chakra-ui/react';
+import { css } from '@emotion/react';
+
+const divider = css`
+  background: gray;
+  cursor: ew-resize;
+  height: 100%;
+  user-select: none; /* ドラッグ中に他領域が選択されるのを抑制 */
+  width: 6px;
+
+  &:hover,
+  &.highlight {
+    background: #62acf5;
+    transition: background 0.2s ease-in;
+  }
+`;
+
+interface Props {
+  onMouseDown: () => void;
+}
+
+export const Divider = ({ onMouseDown }: Props): JSX.Element => {
+  // ドラッグ中にバーからカーソルがずれてもハイライトしたままにしておくために利用
+  const [isPressed, setIsPressed] = useState(false);
+
+  // マウスアップした際にバーからカーソルがずれてても効くように、マウスアップリスナーはドキュメント全体に張る
+  useEffect(() => {
+    const onMouseUp = () => {
+      setIsPressed(false);
+    };
+
+    if (isPressed) {
+      document.addEventListener('mouseup', onMouseUp);
+    }
+
+    return () => {
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+  }, [isPressed, setIsPressed]);
+
+  return (
+    <Box
+      css={divider}
+      className={isPressed ? 'highlight' : ''}
+      onMouseDown={() => {
+        onMouseDown();
+        setIsPressed(true);
+      }}
+    />
+  );
+};
--- src/components/ResizableSidebar/index.tsx
+++ src/components/ResizableSidebar/index.tsx
@@ -1,30 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-import { Box, Flex, Grid } from '@chakra-ui/react';
-import { css } from '@emotion/react';
+import { Flex, Grid } from '@chakra-ui/react';
 
-const divider = css`
-  margin-left: 10px;
-  margin-right: 10px;
-  width: 3px;
-  height: 100%;
-  border-left-width: 1px;
-  border-left-style: solid;
-  border-left-color: black;
-  border-right-width: 1px;
-  border-right-style: solid;
-  border-right-color: black;
-  cursor: ew-resize;
-  user-select: none;
-`;
+import { Divider } from '~/components/ResizableSidebar/Divider';
 
-const ASIDE_MIN_SIZE = 100;
-const ASIDE_MAX_SIZE = 600;
+const ASIDE_INITIAL_SIZE = 300;
+const ASIDE_MIN_SIZE = 150;
+const ASIDE_MAX_SIZE = 500;
 
 export const ResizableSidebar = (): JSX.Element => {
   const isMousePressedRef = useRef(false);
-  const dividerRef = useRef<HTMLDivElement>(null);
-  const [leftW, setLeftW] = useState(200);
+  const asideRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const mouseUpListener = () => {
@@ -33,17 +19,13 @@ export const ResizableSidebar = (): JSX.Element => {
     const mouseMoveListener = (event: MouseEvent) => {
       const x = event.x;
 
-      if (isMousePressedRef.current) {
-        if (dividerRef.current) {
-          if (x < ASIDE_MIN_SIZE) {
-            setLeftW(ASIDE_MIN_SIZE);
-          } else {
-            if (x > ASIDE_MAX_SIZE) {
-              setLeftW(ASIDE_MAX_SIZE);
-            } else {
-              setLeftW(x);
-            }
-          }
+      if (isMousePressedRef.current && asideRef.current !== null) {
+        if (x < ASIDE_MIN_SIZE) {
+          asideRef.current.style.width = `${ASIDE_MIN_SIZE}px`;
+        } else if (x > ASIDE_MAX_SIZE) {
+          asideRef.current.style.width = `${ASIDE_MAX_SIZE}px`;
+        } else {
+          asideRef.current.style.width = `${x}px`;
         }
       }
     };
@@ -59,17 +41,11 @@ export const ResizableSidebar = (): JSX.Element => {
 
   return (
     <Flex w="100vw" h="100vh">
-      <Grid w={`${leftW}px`} h="100%" placeContent="center" fontSize="4xl" bg="green.100">
+      <Grid ref={asideRef} h="100%" w={`${ASIDE_INITIAL_SIZE}px`} placeContent="center" fontSize="4xl" bg="green.100">
         Aside
       </Grid>
-      <Box
-        ref={dividerRef}
-        css={divider}
-        onMouseDown={() => {
-          isMousePressedRef.current = true;
-        }}
-      />
-      <Grid w={`calc(100vw - ${leftW}px)`} h="100%" placeContent="center" fontSize="4xl" bg="purple.100">
+      <Divider onMouseDown={() => (isMousePressedRef.current = true)} />
+      <Grid h="100%" flex="1 1" placeContent="center" fontSize="4xl" bg="purple.100">
         Main
       </Grid>
     </Flex>
