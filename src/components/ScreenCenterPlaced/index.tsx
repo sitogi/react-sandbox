afe16a5b4a638aae24d41df23551cafb83d11e37
sitogi <takano162@gmail.com>
2023-02-26 14:48:42 +0900

    feat: Remove dependencies of chakra

--- src/components/ScreenCenterPlaced/index.tsx
+++ src/components/ScreenCenterPlaced/index.tsx
@@ -1,11 +1,6 @@
+import './index.css';
 import { ReactNode } from 'react';
 
-import { Grid } from '@chakra-ui/react';
-
 export const ScreenCenterPlaced = ({ children }: { children: ReactNode }): JSX.Element => {
-  return (
-    <Grid placeContent="center" h="100vh" w="100vw">
-      {children}
-    </Grid>
-  );
+  return <div className="container">{children}</div>;
 };
