43823473e945a442b159b675e6d98ac74d4bd90e
sitogi <takano162@gmail.com>
2023-05-01 16:43:43 +0900

    feat: removed Screen from ScreenCenterPlaced

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { ScreenCenterPlaced } from '~/featuers/ScreenCenterPlaced';
+import { CenterPlaced } from '~/featuers/CenterPlaced';
 import { SimpleForm } from '~/featuers/SimpleForm';
 
 export function App() {
   return (
-    <ScreenCenterPlaced>
-      <SimpleForm />
-    </ScreenCenterPlaced>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <CenterPlaced>
+        <SimpleForm />
+      </CenterPlaced>
+    </div>
   );
 }
--- src/featuers/CenterPlaced/index.tsx
+++ src/featuers/CenterPlaced/index.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react';
 
 import styles from './index.module.css';
 
-export const ScreenCenterPlaced = ({ children }: { children: ReactNode }): JSX.Element => {
+export const CenterPlaced = ({ children }: { children: ReactNode }): JSX.Element => {
   return <div className={styles['center-placed']}>{children}</div>;
 };
