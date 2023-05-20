61686ae4c3ac47597f0dba9c4bcca8a352c2c075
sitogi <takano162@gmail.com>
2023-05-20 13:53:12 +0900

    Add WithHeaderLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import { FlexWrapSortable } from '~/featuers/FlexWrapSortable';
+import { HorizontallyResizableLayout } from '~/featuers/Resizable/HorizontallyResizableLayout';
+import { WithHeaderLayout } from '~/featuers/WithHeaderLayout';
 
 export function App() {
-  return <FlexWrapSortable />;
+  return (
+    <WithHeaderLayout
+      header={
+        <div style={{ display: 'grid', placeContent: 'center', height: '100%', borderBottom: 'solid 2px gray' }}>
+          <p style={{ fontSize: '2rem' }}>header</p>
+        </div>
+      }
+      content={<HorizontallyResizableLayout />}
+    />
+  );
 }
---
+++ src/featuers/WithHeaderLayout/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  header: ReactNode;
+  content: ReactNode;
+  layoutH?: string;
+  headerH?: string;
+}
+
+export function WithHeaderLayout({ header, content, layoutH = '100vh', headerH = '60px' }: Props): JSX.Element {
+  return (
+    <div style={{ height: layoutH }}>
+      <div style={{ height: headerH }}>{header}</div>
+      <div style={{ height: `calc(${layoutH} - ${headerH})` }}>{content}</div>
+    </div>
+  );
+}
