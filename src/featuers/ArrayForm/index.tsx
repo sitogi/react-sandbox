795a5e2d97557be3e9fe35104db2c6278512f76b
sitogi <takano162@gmail.com>
2023-09-09 21:56:34 +0900

    Summary of field update handlers

--- src/featuers/ArrayForm/index.tsx
+++ src/featuers/ArrayForm/index.tsx
@@ -21,26 +21,16 @@ const INITIAL_PEOPLE: Person[] = [
 export const ArrayForm = (): JSX.Element => {
   const [people, setPeople] = useState<Person[]>(INITIAL_PEOPLE);
 
-  const handleUpdateName = useCallback((id: string, name: string) => {
-    setPeople((current) => current.map((p) => (p.id === id ? { ...p, name } : p)));
-  }, []);
-
-  const handleUpdateAge = useCallback((id: string, age: number) => {
-    setPeople((current) => current.map((p) => (p.id === id ? { ...p, age } : p)));
+  const handleUpdateField = useCallback((id: string, name: string, value: string) => {
+    console.log({ id, name, value });
+    setPeople((current) => current.map((p) => (p.id === id ? { ...p, [name]: value } : p)));
   }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.formsWrapper}>
         {people.map((p) => (
-          <PersonForm
-            key={p.id}
-            id={p.id}
-            name={p.name}
-            age={p.age}
-            updateName={handleUpdateName}
-            updateAge={handleUpdateAge}
-          />
+          <PersonForm key={p.id} id={p.id} name={p.name} age={p.age} updateField={handleUpdateField} />
         ))}
       </div>
     </div>
@@ -51,21 +41,20 @@ type PersonFormProps = {
   id: string;
   name: string;
   age: number;
-  updateName: (id: string, name: string) => void;
-  updateAge: (id: string, age: number) => void;
+  updateField: (id: string, name: string, value: string) => void;
 };
 
-const PersonForm = memo(function PersonForm({ id, name, age, updateName, updateAge }: PersonFormProps): JSX.Element {
+const PersonForm = memo(function PersonForm({ id, name, age, updateField }: PersonFormProps): JSX.Element {
   return (
     <div className={styles.personWrapper}>
       <VStack>
         <div>
           <label htmlFor="name">name:</label>
-          <input id="name" value={name} onChange={(e) => updateName(id, e.target.value)} />
+          <input id="name" name="name" value={name} onChange={(e) => updateField(id, e.target.name, e.target.value)} />
         </div>
         <div>
           <label htmlFor="age">age:</label>
-          <input id="age" value={age} onChange={(e) => updateAge(id, Number(e.target.value))} />
+          <input id="age" name="age" value={age} onChange={(e) => updateField(id, e.target.name, e.target.value)} />
         </div>
       </VStack>
     </div>
