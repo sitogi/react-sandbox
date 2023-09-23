ea062a1d43f3d2199aac355b3b4db960d842b7f9
sitogi <takano162@gmail.com>
2023-09-23 16:12:20 +0900

    Open the browser upon starting the server

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
