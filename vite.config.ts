d2c097b2d93fe3b18bc9253022c3e3f1d07f4ab6
sitogi <takano162@gmail.com>
2023-10-01 11:57:59 +0900

    Add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
@@ -7,7 +8,15 @@ export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '~/': `${__dirname}/src/`, // path.join(__dirname, "src/") でも可
